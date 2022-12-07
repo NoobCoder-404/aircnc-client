c5e9f755d07c67b29428695d6ce575816614de73
Noob-H4cker <alsamiul123@gmail.com>
2022-12-07 17:49:28 +0600

    firebase secure

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,12 +6,12 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBxbiOriFBSKbX57dyLnOwJMiVq_GHVAVo',
-  authDomain: 'aircnc-c9455.firebaseapp.com',
-  projectId: 'aircnc-c9455',
-  storageBucket: 'aircnc-c9455.appspot.com',
-  messagingSenderId: '925469781051',
-  appId: '1:925469781051:web:12fc616118728961ba801e'
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
