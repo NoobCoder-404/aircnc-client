d7842fd9375c468b6165a969e7603db7f3e830b3
Noob-H4cker <alsamiul123@gmail.com>
2022-12-08 14:56:42 +0600

    home card

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ExpCard from '../Components/Card/ExpCard';
+import HomeCard from '../Components/Card/HomeCard';
 import SearchForm from '../Components/Form/SearchForm';
 const Home = () => {
   const [loading, setLoading] = useState(false);
@@ -10,28 +11,43 @@ const Home = () => {
     fetch(`expdata.json`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        //console.log(data);
         setExpCard(data);
       });
   }, []);
   return (
-    <div className="flex px-4">
+    <div className="flex px-4 mt-10">
       <div className="">
         <SearchForm />
       </div>
       <div className="p-6 flex-1 ">
-        <p className="text-2xl pb-3">Home Cards</p>
-        <div className="flex justify-between px-4">
-          <p className="text-xl font-bold">Experiences</p>
-          <Link to={'/comming-soon'} className="">
-            <p className="font-bold text-xl">See All</p>
-          </Link>
-        </div>
-        <div className="container mt-4 mx-auto">
+        <div>
+          <div className="flex justify-between px-4">
+            <p className="text-xl font-bold">Homes</p>
+            <Link to={'/comming-soon'} className="">
+              <p className=" text-lg">See All</p>
+            </Link>
+          </div>
+
           <div className="flex flex-wrap">
-            {expCard.slice(0, 4).map((exp, i) => (
-              <ExpCard key={i} exp={exp}></ExpCard>
+            {[...Array(3)].map((_, i) => (
+              <HomeCard key={i} />
             ))}
+          </div>
+        </div>
+        <div className="mt-10">
+          <div className="flex justify-between px-4">
+            <p className="text-xl font-bold">Experiences</p>
+            <Link to={'/comming-soon'} className="">
+              <p className=" text-lg">See All</p>
+            </Link>
+          </div>
+          <div className="container mt-2 mx-auto">
+            <div className="flex flex-wrap">
+              {expCard.slice(0, 4).map((exp, i) => (
+                <ExpCard key={i} exp={exp}></ExpCard>
+              ))}
+            </div>
           </div>
         </div>
       </div>
